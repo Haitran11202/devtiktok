576a01aaa00b885596036395978003782715f556
Haitran11202 <Haitran11202@gmail.com>
2022-08-06 22:30:03 +0700

    bai 3: tao co che tai layout

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
+import Home from '~/paste/Home';
+import Following from '~/paste/Following';
+import Profile from '~/paste/Profile';
+import Upload from '~/paste/Upload';
+import Search from '~/paste/Search';
+//public route
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile, layout: null },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/search', component: Search, layout: null },
+];
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
